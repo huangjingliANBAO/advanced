e67244cc301363972d13a3db58f6ad9d127361f7
黄敬理 <jingliqaq@gmail.com>
2019-04-10 07:21:22 +0800

    add

---
+++ thread-study/src/week1/com/soft1841/thread/DrawLineFrame.java
@@ -0,0 +1,31 @@
+package week1.com.soft1841.thread;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * 线程学习 绘制彩色线段
+ * @author 黄敬理
+ * 2019.04.10
+ */
+public class DrawLineFrame extends JFrame {
+    public DrawLineFrame(){
+        init();
+        setTitle("绘制彩色线段");
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+    private void init(){
+        getContentPane().setBackground(new Color(87, 201, 185));
+        DrawLineThread drawLineThread = new DrawLineThread();
+        drawLineThread.setFrame(this);
+        new Thread(drawLineThread).start();
+    }
+
+    public static void main(String[] args) {
+        new DrawLineFrame();
+    }
+
+}
---
+++ thread-study/src/week1/com/soft1841/thread/DrawLineThread.java
@@ -0,0 +1,46 @@
+package week1.com.soft1841.thread;
+
+import org.omg.PortableServer.THREAD_POLICY_ID;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * 线程学习 绘制彩色线段
+ * @author 黄敬理
+ * 2019.04.10
+ */
+public class DrawLineThread implements Runnable {
+    int x = 100;
+    int y = 100;
+    private JFrame frame;
+    private Color[] colors = {Color.ORANGE,Color.ORANGE,Color.ORANGE,Color.ORANGE,Color.GREEN,Color.GREEN,
+            Color.GREEN,Color.GREEN,Color.ORANGE,Color.RED,Color.RED,Color.RED,Color.RED,Color.RED
+    };
+    public void setFrame(JFrame frame){
+        this.frame = frame;
+    }
+    @Override
+    public void run() {
+        Random random = new Random();
+        while (true){
+            try {
+                Thread.sleep(500);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Graphics graphics = frame.getGraphics();
+            Graphics2D g2d=(Graphics2D)graphics;
+            Stroke stroke=new BasicStroke(5.0f);//设置线宽为3.0
+            g2d.setStroke(stroke);
+            graphics.setColor(colors[random.nextInt(colors.length)]);
+            graphics.drawLine(x,y,590,y);
+            y += 50;
+            if (y>=500){
+                y=100;
+            }
+        }
+    }
+}
