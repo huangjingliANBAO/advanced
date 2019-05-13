72878c34d6a009b947233500ccfd8c558b278194
黄敬理 <jingliqaq@gmail.com>
2019-05-13 09:27:28 +0800

    add gui

---
+++ gui-form/src/gui1/TimLoginFream.java
@@ -0,0 +1,19 @@
+package gui1;
+
+import javax.swing.*;
+
+public class TimLoginFream {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JCheckBox 记住密码CheckBox;
+    private JCheckBox 自动登录CheckBox;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("TimLoginFream");
+        frame.setContentPane(new TimLoginFream().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
